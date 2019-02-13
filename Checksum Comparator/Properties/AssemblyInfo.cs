6f6ed14c86681c62d85bf6223ade7c56718c3e65
TDL3 <jacksimpkins7@gmail.com>
2019-02-14 02:40:51 +0800

    update assembly info

--- Checksum Comparator/Properties/AssemblyInfo.cs
+++ Checksum Comparator/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Checksum Comparator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Yet another checksum comparison tool")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("TDL3.worldpress.com")]
 [assembly: AssemblyProduct("Checksum Comparator")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © TDL3 2019")]
+[assembly: AssemblyTrademark("TDL3.worldpress.com")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
